064526328d18c5da4886e2de31dc4d86d3c3a6e5
Sandu-2002 <sanjayapraveen2@gmail.com>
2026-03-21 21:57:15 +0530

    database connect

--- backend/src/db/schema.js
+++ backend/src/db/schema.js
@@ -2,4 +2,11 @@ import {pgTable, serial, text, timestamp, integer} from 'drizzle-orm/pg-core';
 
 export const favoritesTable = pgTable('favorites', {
     id: serial('id').primaryKey(),
+    userId: text('user_id').notNull(), 
+    recipeId: integer('recipe_id').notNull(),
+    createdAt: timestamp('created_at').defaultNow(),
+    title : text('title').notNull(),
+    Image: text('image').notNull(),
+    cookTime: text('cook_time').notNull(),
+    servings: text('servings').notNull(),      
 });
