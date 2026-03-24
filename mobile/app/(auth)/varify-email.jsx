c85964562501c0e7991dfb8dbe0d1d528b902fa0
Sandu-2002 <sanjayapraveen2@gmail.com>
2026-03-24 22:55:25 +0530

    image add

--- mobile/app/(auth)/varify-email.jsx
+++ mobile/app/(auth)/varify-email.jsx
@@ -1,9 +1,55 @@
-import { View, Text } from 'react-native'
+import { View, Text, Alert, KeyboardAvoidingView, ScrollView, Image, Platform } from 'react-native'
 import React from 'react'
+import { useState } from 'react'
+import { useSignUp } from "@clerk/expo";
+import { authStyles } from '../../assets/styles/auth.styles'
 
-const VerifyEmail = () => {
+const VerifyEmail = ({ email, onBack }) => {
+  const {isLoaded, signUp, setActive} =useSignUp();
+  const [code, setCode] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleVerificaction = async () => {
+    if(!isLoaded) return;
+
+    setLoading (true)
+    try {
+      const signUpAttempt = await signUp.attemptEmailAddressVerification({code});
+      if (signUpAttempt.status === 'complete') {
+       await setActive({session: signUpAttempt.createdSessionId});
+      }else{
+        Alert.alert("Error", "Verification failed. Please try again.");
+        console.error(JSON.stringify(signUpAttempt, null, 2));
+      }
+    } 
+    catch (err) {
+      Alert.alert("Error", err.errors?.[0]?.message || "Verification failed");
+      console.error(JSON.stringify(err, null, 2));
+    }
+    finally {
+      setLoading(false);
+    }
+  }
+
   return (
-    <View>
+    <View style={authStyles.container}>
+      <KeyboardAvoidingView
+      behavior={Platform.OS === "android" ? "padding" : "height"}
+      keyboardVerticalOffset={Platform.OS === "android" ? 25 : 0}>  
+      
+      <ScrollView
+      content ContainerStyle={authStyles.scrollContent}
+      showsVerticalScrollIndicator={false}>
+        {/*image container*/}
+        <View style={authStyles.imageContainer}>
+          <Image 
+          source={require("../../assets/images/i3.png")}
+          style={authStyles.image}
+          contentFit="contain"/>
+        </View>
+
+        </ScrollView>
+      </KeyboardAvoidingView>
       <Text>VerifyEmail</Text>
     </View>
   )
