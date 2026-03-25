5617d2c929a426894a215732d68b1b8d4d34b8a6
Sandu-2002 <sanjayapraveen2@gmail.com>
2026-03-25 20:03:37 +0530

    meal api connect and and make screnns

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,8 +1,6 @@
 import { ClerkProvider } from '@clerk/expo';
 import { tokenCache } from '@clerk/expo/token-cache';
 import { Slot } from 'expo-router'
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { COLORS } from '../constants/colors'
 import SafeScreen from "@/components/SafeScreen"
 
 export default function RootLayout() {
